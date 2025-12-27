eff067665e8747aa1526e29426bd8ba039ddcbf6
Rudra08saini <rudra08saini@gmail.com>
2025-12-27 15:00:26 +0530

    EDA and Model Training

--- src/logger.py
+++ src/logger.py
@@ -12,6 +12,3 @@ logging.basicConfig(
     level=logging.INFO,
     format='[%(asctime)s] %(lineno)d %(name)s  %(levelname)s - %(message)s',
 ) 
-
-if __name__ == "__main__":
-    logging.info("Logging has been configured.")
