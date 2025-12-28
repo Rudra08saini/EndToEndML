c53bbf53f1162f5801fee6ae48697b08967ce28f
Rudra08saini <rudra08saini@gmail.com>
2025-12-28 12:03:43 +0530

    Hyperparameter Tuning

--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -36,11 +36,46 @@ class ModelTrainer:
                 "Linear Regression": LinearRegression(),
                 "KNeighbors Regressor": KNeighborsRegressor(),
                 "XGB Regressor": XGBRegressor(),
-                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
-            
-            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+            params ={
+                
+                "Random Forest": {
+                    'n_estimators':[8,16,32,64,128,256],
+                    'max_features':['sqrt','log2',None],
+                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson']
+                    
+                },
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'splitter':['best','random'] ,
+                    'max_features':['sqrt','log2']
+                },
+                "Gradient Boosting": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators':[8,16,32,64,128,256],
+                    'max_features':['sqrt','log2',None],
+                    'loss':['squared_error', 'huber', 'absolute_error', 'quantile']
+                },
+                "Linear Regression": {},
+                "KNeighbors Regressor": {
+                    'n_neighbors':[5,7,9,11],
+                    'weights':['uniform','distance']
+                },
+                "XGB Regressor": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                
+                
+                "AdaBoost Regressor": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                }
+            }
+            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models , param=params)
             
             # To get the best model score from the dictionary
             best_model_score = max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -5,26 +5,38 @@ from src.logger import logging
 import pickle
 import numpy as np
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import StratifiedKFold
 def save_object(file_path, obj):
     try:
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
-        with open(file_path, "wb") as file_obj:
+        with open(file_path, "wb") as file_obj: 
             pickle.dump(obj, file_obj)
     except Exception as e:
         logging.error("Error occurred while saving object")
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models , param):
     try:
         report = {}
-        for i in range(len(models)):
+        skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+        for i in range(len(list(models))):
             model = list(models.values())[i]
+            params = param[list(models.keys())[i]]
+            
+            gs = GridSearchCV(model, params, cv=skf, n_jobs=-1, verbose=0)
+            gs.fit(X_train, y_train)
+            
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
+            
             y_test_pred = model.predict(X_test)
+            
             test_model_score = r2_score(y_test, y_test_pred)
+            
             report[list(models.keys())[i]] = test_model_score
+            
         return report
     except Exception as e:
         logging.error("Error occurred during model evaluation")
-        raise CustomException(e, sys)    
+        raise CustomException(e, sys)  
